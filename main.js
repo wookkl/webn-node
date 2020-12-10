ed0058b3a62b58f83324309a99d4121b2c622408
wookkl <wjddnr315@gmail.com>
2020-12-10 15:19:48 +0900

    homepage

--- main.js
+++ main.js
@@ -1,17 +1,80 @@
-var http = require('http');
-var fs = require('fs');
-var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const app = http.createServer(function(request,response){
+    const _url = request.url;
+    const queryData = url.parse(_url, true).query;
+    const pathName = url.parse(_url, true).pathname;
+    
+    if(pathName === '/') {
+      if(queryData.id === undefined) {
+        title = 'Welcome';
+        description = 'Hellow, Node js';
+        const template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <input id="night_day" type="button" value="night" onclick="
+                nightDayHandler(this);
+              ">
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2> 
+              <article>
+              ${description}
+              </article>
+            </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+          const title = queryData.id;
+          const template = `
+          <!doctype html>
+          <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              <input id="night_day" type="button" value="night" onclick="
+                nightDayHandler(this);
+              ">
+              <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+              </ol>
+              <h2>${title}</h2> 
+              <article>
+              ${description}
+              </article>
+            </body>
+          </html>
+        `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      }
+      
+
+    } else {
+      response.writeHead(404);
+      response.end(`404 not found`);
     }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    
+    
  
 });
 app.listen(3000);
